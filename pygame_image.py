33efaca09c54f736bf44f8234dfe2d4fa2ef98f5
C0A23137 <c0a2313797@edu.teu.ac.jp>
2024-11-19 17:21:31 +0900

    move_ipメソッドに使用１回

--- pygame_image.py
+++ pygame_image.py
@@ -23,11 +23,11 @@ def main():
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
-        if key_lst[pg.K_DOWN]:
+        elif key_lst[pg.K_DOWN]:
             kk_rct.move_ip((0, 1))
-        if key_lst[pg.K_LEFT]:
+        elif key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
-        if key_lst[pg.K_RIGHT]:
+        elif key_lst[pg.K_RIGHT]:
             kk_rct.move_ip((1, 0))
         else:
             kk_rct.move_ip(-1, 0)  # キーが押されていない場合は左に移動
